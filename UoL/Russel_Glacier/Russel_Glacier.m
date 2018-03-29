376f9ce8e88d398b365897bce63556643440a57d
adrianoglemos <adrianoglemos@yahoo.com.br>
2018-03-29 13:45:22 +0100

    Added the 2017 organization

--- UoL/Russel_Glacier/Russel_Glacier.m
+++ UoL/Russel_Glacier/Russel_Glacier.m
@@ -130,6 +130,73 @@ for i=1:size(list_opt,1);
 i
 end
 
+%%%%%
+clear all
+%%%%%
+
+pathsate_opt=['/nfs/a59/eeagdl/Data/Available_Images/GRIS_Mosaic_test/Russel_Glacier/2017/'];
+
+d_opt=dir(pathsate_opt);
+j=1;
+for i=6:length(d_opt);
+    name=d_opt(i,1).name;
+    data{j,1}=cellstr(name);
+    %date{j,1}=cellstr(name(18:25));
+    list_opt(j,1)=cellstr(data{j,1});
+    j=j+1;
+%folder=char(folder, data);
+end
+clear d_opt i j name
+
+% Sort dates
+
+for i=1:size(list_opt,1);
+    a1=list_opt{i,1};
+    date_2017(i,:)=a1(18:25);
+    
+    date_2017_num(i,1)=datenum(a1(18:25),'yyyymmdd');
+    
+end
+
+
+[date_2017_num_sort,I_sort]=sort(date_2017_num);
+
+date_2017_sort=date_2017(I_sort,:);
+
+%%%%%
+% Create folder and copy the image ;
+%%%%%
+
+for i=1:size(date_2017_sort,1)-1;
+    %path=[pathsate_opt];
+%     path1=[pathsate_opt list_opt{i,1} '/'];
+%     path2=[pathsate_opt list_opt{i+1,1} '/'];
+    
+    
+    date1=date_2017_sort(i,:);
+    date2=date_2017_sort(i+1,:);
+
+    cd(pathsate_opt)
+    %path1=[pathsate_opt1 list_opt{i,1} '/'];
+    %path2=[pathsate_opt2 list_opt{i,1} '/'];
+  eval(['!mkdir ./' date1 '_' date2]); 
+
+  cd(['./' date1 '_' date2])
+  
+  eval(['!cp ../*' date1 '*zip ./'])
+  eval(['!cp ../*' date2 '*zip ./'])
+  
+    %date=list_opt{i,1};
+
+    %cd(path1);
+
+ %eval( ['!mv ' path '*.zip ' destination]);
+ %eval( ['!mv ' path2 '*' date2 '*.zip ' path]);
+
+i
+end
+
+
 
 
 
